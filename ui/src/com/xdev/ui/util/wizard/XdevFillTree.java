037eab8fb2fd640b1564535951b00337d3fe2921
FH <f.habermann@xdev-software.de>
2015-03-10 19:28:02 +0100

    Refactorings

--- ui/src/com/xdev/ui/util/wizard/XdevFillTree.java
+++ ui/src/com/xdev/ui/util/wizard/XdevFillTree.java
@@ -6,10 +6,10 @@ import java.util.Collection;
 
 import com.vaadin.data.util.HierarchicalContainer;
 import com.vaadin.ui.AbstractSelect;
+import com.xdev.server.util.Caption;
 import com.xdev.server.util.EntityReferenceResolver;
 import com.xdev.server.util.HibernateEntityReferenceResolver;
-import com.xdev.server.util.annotation.Caption;
-import com.xdev.server.util.annotation.EntityFieldAnnotation;
+import com.xdev.server.util.ReflectionUtils;
 import com.xdev.ui.entitycomponent.hierarchical.FillTree;
 import com.xdev.ui.entitycomponent.hierarchical.Group;
 
@@ -19,9 +19,8 @@ public class XdevFillTree implements XdevExecutableCommandObject
 {
 	private final FillTree					fillTreeComposite;
 	private final EntityReferenceResolver	referenceResolver;
-	private final EntityFieldAnnotation		captionAnnotation;
-	
-	
+
+
 	/**
 	 *
 	 */
@@ -31,22 +30,21 @@ public class XdevFillTree implements XdevExecutableCommandObject
 		this.fillTreeComposite = new FillTree.Implementation();
 		this.fillTreeComposite.setHierarchicalReceiver(tree);
 		this.referenceResolver = new HibernateEntityReferenceResolver();
-		this.captionAnnotation = new EntityFieldAnnotation();
 	}
-	
-	
+
+
 	/*
 	 * --------------- UTILITY DELEGATORS -------------------
 	 */
-	
+
 	// TODO create tailored exception type
 	public <T> Group addRootGroup(final Class<T> clazz) throws RuntimeException
 	{
 		return this.fillTreeComposite.addRootGroup(clazz,
-				this.captionAnnotation.getAnnotationField(clazz,Caption.class));
+				ReflectionUtils.getAnnotatedField(clazz,Caption.class));
 	}
-	
-	
+
+
 	// TODO create tailored exception type
 	public Group addGroup(final Class<?> clazz, final Class<?> parentClazz) throws RuntimeException
 	{
@@ -56,34 +54,34 @@ public class XdevFillTree implements XdevExecutableCommandObject
 		try
 		{
 			return this.fillTreeComposite.addGroup(clazz,clazz.getDeclaredField(propertyName),
-					this.captionAnnotation.getAnnotationField(clazz,Caption.class));
+					ReflectionUtils.getAnnotatedField(clazz,Caption.class));
 		}
 		catch(NoSuchFieldException | SecurityException e)
 		{
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
+
+
 	public <T> void setGroupData(final Class<T> groupClass, final Collection<T> data)
 	{
 		this.fillTreeComposite.setGroupData(groupClass,data);
 	}
-	
-	
+
+
 	@Override
 	public void execute()
 	{
 		this.fillTreeComposite.fillTree(new HierarchicalContainer());
 	}
-	
-	
+
+
 	public void setHierarchicalReceiver(final AbstractSelect treeComponent)
 	{
 		this.fillTreeComposite.setHierarchicalReceiver(treeComponent);
 	}
-	
-	
+
+
 	public AbstractSelect getHierarchicalReceiver()
 	{
 		return this.fillTreeComposite.getHierarchicalReceiver();

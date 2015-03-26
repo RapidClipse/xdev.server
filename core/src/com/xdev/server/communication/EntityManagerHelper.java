ea683659e64d68efdb8895e6695495611fea1eb3
FH <f.habermann@xdev-software.de>
2015-03-26 14:53:17 +0100

    Moved license prepender to maven

--- core/src/com/xdev/server/communication/EntityManagerHelper.java
+++ core/src/com/xdev/server/communication/EntityManagerHelper.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
 package com.xdev.server.communication;
 
 
@@ -31,81 +30,81 @@ import com.xdev.server.db.connection.HibernateUtil;
 // Vaadin Session / technology - specific global helper
 public class EntityManagerHelper
 {
-	
+
 	private static EntityManagerFactory	emf;
-	
-	
+
+
 	public static EntityManagerFactory getEntityManagerFactory()
 	{
 		return emf;
 	}
-	
+
 	private static HibernateUtil	hibernateUtil;
-	
-	
-	public static EntityManagerFactory initializeHibernateFactory(HibernateUtil hibernateUtil)
+
+
+	public static EntityManagerFactory initializeHibernateFactory(final HibernateUtil hibernateUtil)
 	{
 		EntityManagerHelper.hibernateUtil = hibernateUtil;
 		return emf = hibernateUtil.getEntityManagerFactory();
 	}
-	
-	
+
+
 	public static HibernateUtil getHibernateutil()
 	{
 		return hibernateUtil;
 	}
-	
-	
+
+
 	public static EntityManager getEntityManager()
 	{
 		return (EntityManager)VaadinSession.getCurrent().getAttribute("HibernateEntityManager");
 	}
-	
-	
+
+
 	public static void closeEntityManager()
 	{
-		EntityManager entityManager = getEntityManager();
+		final EntityManager entityManager = getEntityManager();
 		if(entityManager != null)
 		{
 			entityManager.close();
 		}
 	}
-	
-	
+
+
 	public static void closeEntityManagerFactory()
 	{
 		emf.close();
 	}
-	
-	
+
+
 	public static void beginTransaction()
 	{
 		getEntityManager().getTransaction().begin();
 	}
-	
-	
+
+
 	public static void rollback()
 	{
 		getEntityManager().getTransaction().rollback();
 	}
-	
-	
+
+
 	public static void commit()
 	{
 		getEntityManager().getTransaction().commit();
 	}
-	
-	
+
+
 	public static EntityTransaction getTransaction()
 	{
 		return getEntityManager().getTransaction();
 	}
-	
-	
-	public static <T> CriteriaQuery<T> getCriteriaQuery(Class<T> type)
+
+
+	public static <T> CriteriaQuery<T> getCriteriaQuery(final Class<T> type)
 	{
-		CriteriaBuilder cb = EntityManagerHelper.getEntityManager().getCriteriaBuilder();
-		CriteriaQuery<T> cq = cb.createQuery(type);
+		final CriteriaBuilder cb = EntityManagerHelper.getEntityManager().getCriteriaBuilder();
+		final CriteriaQuery<T> cq = cb.createQuery(type);
 		return cq;
 	}
 }
--- core/src/com/xdev/server/util/AccessUtils.java
+++ core/src/com/xdev/server/util/AccessUtils.java
@@ -1,4 +1,20 @@
-
+/*
+ * Copyright (C) 2013-2015 by XDEV Software, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 package com.xdev.server.util;
 
 
@@ -17,30 +33,32 @@ public class AccessUtils
 {
 	public static Future<Void> access(final Runnable runnable)
 	{
-		EntityManagerFactory factory = EntityManagerHelper.getEntityManagerFactory();
+		final EntityManagerFactory factory = EntityManagerHelper.getEntityManagerFactory();
 		if(factory != null)
 		{
-			EntityManager manager = factory.createEntityManager();
+			final EntityManager manager = factory.createEntityManager();
 			// Add the EntityManager to the session
 			VaadinSession.getCurrent().setAttribute("HibernateEntityManager",manager);
 		}
-		
-		Future<Void> future = UI.getCurrent().access(runnable);
-		
+
+		final Future<Void> future = UI.getCurrent().access(runnable);
+
 		try
 		{
 			EntityManagerHelper.closeEntityManager();
 		}
-		catch(Exception e)
+		catch(final Exception e)
 		{
 			if(EntityManagerHelper.getEntityManager() != null)
 			{
-				EntityTransaction tx = EntityManagerHelper.getTransaction();
+				final EntityTransaction tx = EntityManagerHelper.getTransaction();
 				if(tx != null && tx.isActive())
+				{
 					EntityManagerHelper.rollback();
+				}
 			}
 		}
-		
+
 		return future;
 	}
 }

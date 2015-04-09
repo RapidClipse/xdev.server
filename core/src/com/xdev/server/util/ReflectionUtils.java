67c3ee66513e8fafe3d0e928f8a484fcaabbd620
FH <f.habermann@xdev-software.de>
2015-04-09 13:21:18 +0200

    ReflectionUtils rework

--- core/src/com/xdev/server/util/ReflectionUtils.java
+++ core/src/com/xdev/server/util/ReflectionUtils.java
@@ -5,16 +5,16 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 package com.xdev.server.util;
 
 
@@ -27,21 +27,20 @@ public final class ReflectionUtils
 	private ReflectionUtils()
 	{
 	}
-	
-	
+
+
 	public static <T extends Annotation> Field getAnnotatedField(final Class<?> ownerClass,
 			final Class<T> annotationClass)
 	{
-		for(int i = 0; i < ownerClass.getDeclaredFields().length; i++)
+		for(final Field field : ownerClass.getDeclaredFields())
 		{
-			final Field f = ownerClass.getDeclaredFields()[i];
-			final T a = f.getDeclaredAnnotation(annotationClass);
+			final T a = field.getDeclaredAnnotation(annotationClass);
 			if(a != null)
 			{
-				return f;
+				return field;
 			}
 		}
-		
+
 		return null;
 	}
 }

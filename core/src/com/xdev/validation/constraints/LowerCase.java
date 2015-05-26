5a387b30ae3bc5b2b85837485b0fc98cd5a0c0f4
ano46 <ano46@web.de>
2015-05-26 13:38:10 +0200

    Added validators

---
+++ core/src/com/xdev/validation/constraints/LowerCase.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2013-2015 by XDEV Software, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.xdev.validation.constraints;
+
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.xdev.validation.constraints.validators.UpperCaseValidator;
+
+
+/**
+ * @author XDEV Software
+ *
+ */
+
+@Target({METHOD,FIELD,ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = UpperCaseValidator.class)
+@Documented
+public @interface LowerCase
+{
+	// String message() default "{com.xdev.constraints.lowercase}";
+	
+	Class<?>[] groups() default {};
+	
+	
+	Class<? extends Payload>[] payload() default {};
+}

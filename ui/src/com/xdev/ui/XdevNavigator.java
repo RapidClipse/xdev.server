3c0a58f093b87a3c05ae6425beb396e307ec10f1
FH <f.habermann@xdev-software.de>
2015-07-15 17:13:43 +0200

    XdevNavigator extends now Navigator, no own implementation.

--- ui/src/com/xdev/ui/XdevNavigator.java
+++ ui/src/com/xdev/ui/XdevNavigator.java
@@ -18,21 +18,15 @@
 package com.xdev.ui;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.vaadin.navigator.NavigationStateManager;
 import com.vaadin.navigator.Navigator;
-import com.vaadin.navigator.Navigator.SingleComponentContainerViewDisplay;
 import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.navigator.ViewProvider;
-import com.vaadin.server.ClientConnector.AttachEvent;
-import com.vaadin.server.ClientConnector.AttachListener;
+import com.vaadin.navigator.ViewDisplay;
+import com.vaadin.server.Page;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.SingleComponentContainer;
+import com.vaadin.ui.UI;
 
 
 /**
@@ -44,514 +38,111 @@ import com.vaadin.ui.SingleComponentContainer;
  * an optional parameter string that is included in the fragment (may be
  * bookmarkable).
  * <p>
- * The views are displayed in the attached container, see
- * {@link #setContainer(SingleComponentContainer)}.
- *
- * @author XDEV Software
+ * Views can be explicitly registered or dynamically generated and listening to
+ * view changes is possible.
+ * <p>
+ * Note that {@link XdevNavigator} is not a component itself but uses a
+ * {@link ViewDisplay} to update contents based on the state.
  */
-public class XdevNavigator
+public class XdevNavigator extends Navigator
 {
-	protected SingleComponentContainer		container;
-	protected Navigator						navigator;
-	protected final Map<String, ViewEntry>	views	= new HashMap<>();
-	protected List<ViewProvider>			additionalViewProviders;
-	protected String						initialNavigationState;
-	protected ViewEntry						errorView;
-	protected List<ViewChangeListener>		viewChangeListeners;
-
-
 	/**
+	 * Creates a navigator that is tracking the active view using URI fragments
+	 * of the {@link Page} containing the given UI and replacing the contents of
+	 * a {@link ComponentContainer} with the active view.
+	 * <p>
+	 * All components of the container are removed each time before adding the
+	 * active {@link View}. Views must implement {@link Component} when using
+	 * this constructor.
+	 * <p>
+	 * Navigation is automatically initiated after {@code UI.init()}Â if a
+	 * navigator was created. If at a later point changes are made to the
+	 * navigator, {@code navigator.navigateTo(navigator.getState())} may need to
+	 * be explicitly called to ensure the current view matches the navigation
+	 * state.
 	 *
-	 */
-	public XdevNavigator()
-	{
-		this.views.put("",new XdevViewEntry("",XdevView.class,""));
-	}
-
-
-	/**
-	 * @return the container in which the views are displayed
-	 */
-	public SingleComponentContainer getContainer()
-	{
-		return this.container;
-	}
-
-
-	/**
+	 * @param ui
+	 *            The UI to which this Navigator is attached.
 	 * @param container
-	 *            the container in which the views are displayed
+	 *            The ComponentContainer whose contents should be replaced with
+	 *            the active view on view change
 	 */
-	public void setContainer(final SingleComponentContainer container)
+	public XdevNavigator(final UI ui, final ComponentContainer container)
 	{
-		this.container = container;
-
-		container.addAttachListener(new AttachListener()
-		{
-			@Override
-			public void attach(final AttachEvent event)
-			{
-				container.removeAttachListener(this);
-				XdevNavigator.this.attach();
-			}
-		});
-	}
-
-
-	protected void attach()
-	{
-		this.navigator = createNavigator();
-		this.navigator.addProvider(new XdevViewProvider());
-
-		if(this.additionalViewProviders != null)
-		{
-			for(final ViewProvider viewProvider : this.additionalViewProviders)
-			{
-				this.navigator.addProvider(viewProvider);
-			}
-			this.additionalViewProviders = null;
-		}
-
-		if(this.errorView != null)
-		{
-			navigator_setErrorView(this.errorView);
-			this.errorView = null;
-		}
-
-		if(this.initialNavigationState != null)
-		{
-			this.navigator.navigateTo(this.initialNavigationState);
-			this.initialNavigationState = null;
-		}
-
-		if(this.viewChangeListeners != null)
-		{
-			for(final ViewChangeListener listener : this.viewChangeListeners)
-			{
-				this.navigator.addViewChangeListener(listener);
-			}
-			this.viewChangeListeners = null;
-		}
-	}
-
-
-	protected Navigator createNavigator()
-	{
-		return new Navigator(this.container.getUI(),new SingleComponentContainerViewDisplay(
-				this.container));
+		super(ui,container);
 	}
 
 
 	/**
-	 * Registers a view class for a view name.
+	 * Creates a navigator.
 	 * <p>
-	 * Registering another view with a name that is already registered
-	 * overwrites the old registration of the same type.
+	 * When a custom navigation state manager is not needed, use one of the
+	 * other constructors which use a URI fragment based state manager.
 	 * <p>
-	 * A new view instance is created every time a view is requested.
+	 * Navigation is automatically initiated after {@code UI.init()}Â if a
+	 * navigator was created. If at a later point changes are made to the
+	 * navigator, {@code navigator.navigateTo(navigator.getState())} may need to
+	 * be explicitly called to ensure the current view matches the navigation
+	 * state.
 	 *
-	 * @param path
-	 *            String that identifies a view (not null nor empty string)
-	 * @param viewType
-	 *            {@link View} class to instantiate when a view is requested
-	 *            (not null)
+	 * @param ui
+	 *            The UI to which this Navigator is attached.
+	 * @param stateManager
+	 *            The NavigationStateManager keeping track of the active view
+	 *            and enabling bookmarking and direct navigation
+	 * @param display
+	 *            The ViewDisplay used to display the views handled by this
+	 *            navigator
 	 */
-	public void addView(final String path, final Class<? extends View> viewType)
+	public XdevNavigator(final UI ui, final NavigationStateManager stateManager,
+			final ViewDisplay display)
 	{
-		addView(path,viewType,path);
+		super(ui,stateManager,display);
 	}
 
 
 	/**
-	 * Registers a view class for a view name.
+	 * Creates a navigator that is tracking the active view using URI fragments
+	 * of the {@link Page} containing the given UI and replacing the contents of
+	 * a {@link SingleComponentContainer} with the active view.
 	 * <p>
-	 * Registering another view with a name that is already registered
-	 * overwrites the old registration of the same type.
+	 * Views must implement {@link Component} when using this constructor.
 	 * <p>
-	 * A new view instance is created every time a view is requested.
+	 * Navigation is automatically initiated after {@code UI.init()}Â if a
+	 * navigator was created. If at a later point changes are made to the
+	 * navigator, {@code navigator.navigateTo(navigator.getState())} may need to
+	 * be explicitly called to ensure the current view matches the navigation
+	 * state.
 	 *
-	 * @param path
-	 *            String that identifies a view (not null nor empty string)
-	 * @param view
-	 *            {@link View} class to instantiate when a view is requested
-	 *            (not null)
-	 * @param optional
-	 *            title title of the view
+	 * @param ui
+	 *            The UI to which this Navigator is attached.
+	 * @param container
+	 *            The SingleComponentContainer whose contents should be replaced
+	 *            with the active view on view change
 	 */
-	public void addView(final String path, final Class<? extends View> viewType, final String title)
+	public XdevNavigator(final UI ui, final SingleComponentContainer container)
 	{
-		this.views.put(path,new XdevViewEntry(path,viewType,title));
+		super(ui,container);
 	}
 
 
 	/**
-	 * Registers a view class for a view name.
+	 * Creates a navigator that is tracking the active view using URI fragments
+	 * of the {@link Page} containing the given UI.
 	 * <p>
-	 * Registering another view with a name that is already registered
-	 * overwrites the old registration of the same type.
-	 * <p>
-	 * A new view instance is created every time a view is requested.
+	 * Navigation is automatically initiated after {@code UI.init()}Â if a
+	 * navigator was created. If at a later point changes are made to the
+	 * navigator, {@code navigator.navigateTo(navigator.getState())} may need to
+	 * be explicitly called to ensure the current view matches the navigation
+	 * state.
 	 *
-	 * @param path
-	 *            String that identifies a view (not null nor empty string)
-	 * @param view
-	 *            {@link View} the view to display (not null)
+	 * @param ui
+	 *            The UI to which this Navigator is attached.
+	 * @param display
+	 *            The ViewDisplay used to display the views.
 	 */
-	public <V extends Component & View> void addView(final String path, final V view)
+	public XdevNavigator(final UI ui, final ViewDisplay display)
 	{
-		addView(path,view,path);
-	}
-
-
-	/**
-	 * Registers a view for a view name.
-	 * <p>
-	 * Registering another view with a name that is already registered
-	 * overwrites the old registration of the same type.
-	 * <p>
-	 * A new view instance is created every time a view is requested.
-	 *
-	 * @param path
-	 *            String that identifies a view (not null nor empty string)
-	 * @param view
-	 *            {@link View} the view to display (not null)
-	 * @param optional
-	 *            title title of the view
-	 */
-	public <V extends Component & View> void addView(final String path, final V view,
-			final String title)
-	{
-		this.views.put(path,new XdevViewEntry(path,view,title));
-	}
-
-
-	public ViewEntry removeView(final String path)
-	{
-		return this.views.remove(path);
-	}
-
-
-	/**
-	 *
-	 * @return all registered views
-	 * @see #addView(String, Class)
-	 * @see #addView(String, Class, String)
-	 */
-	public ViewEntry[] getViews()
-	{
-		return this.views.values().toArray(new XdevViewEntry[this.views.size()]);
-	}
-
-
-	/**
-	 * Registers a view that is instantiated when no other view matches the
-	 * navigation state.
-	 *
-	 * @param viewType
-	 */
-	public void setErrorView(final Class<? extends View> viewType)
-	{
-		setErrorView(new XdevViewEntry("",viewType,""));
-	}
-
-
-	/**
-	 * Registers a view that is instantiated when no other view matches the
-	 * navigation state.
-	 *
-	 * @param view
-	 */
-	public <V extends Component & View> void setErrorView(final V view)
-	{
-		setErrorView(new XdevViewEntry("",view,""));
-	}
-
-
-	private void setErrorView(final ViewEntry errorView)
-	{
-		if(this.navigator != null)
-		{
-			navigator_setErrorView(errorView);
-		}
-		else
-		{
-			this.errorView = errorView;
-		}
-	}
-
-
-	private void navigator_setErrorView(final ViewEntry errorView)
-	{
-		this.navigator.setErrorProvider(new ViewProvider()
-		{
-			@Override
-			public String getViewName(final String viewAndParameters)
-			{
-				return viewAndParameters;
-			}
-
-
-			@Override
-			public View getView(final String viewName)
-			{
-				return errorView.getView();
-			}
-		});
-	}
-
-
-	public void removeErrorView()
-	{
-		if(this.navigator != null)
-		{
-			this.navigator.setErrorProvider(null);
-		}
-		else
-		{
-			this.errorView = null;
-		}
-	}
-
-
-	public void addViewProvider(final ViewProvider viewProvider)
-	{
-		if(this.navigator != null)
-		{
-			this.navigator.addProvider(viewProvider);
-		}
-		else
-		{
-			if(this.additionalViewProviders == null)
-			{
-				this.additionalViewProviders = new ArrayList<>();
-			}
-
-			this.additionalViewProviders.add(viewProvider);
-		}
-	}
-
-
-	public void removeViewProvider(final ViewProvider viewProvider)
-	{
-		if(this.navigator != null)
-		{
-			this.navigator.removeProvider(viewProvider);
-		}
-		else if(this.additionalViewProviders != null)
-		{
-			this.additionalViewProviders.remove(viewProvider);
-
-			if(this.additionalViewProviders.isEmpty())
-			{
-				this.additionalViewProviders = null;
-			}
-		}
-	}
-
-
-	/**
-	 * Listen to changes of the active view.
-	 * <p>
-	 * Registered listeners are invoked in registration order before (
-	 * {@link ViewChangeListener#beforeViewChange(ViewChangeEvent)
-	 * beforeViewChange()}) and after (
-	 * {@link ViewChangeListener#afterViewChange(ViewChangeEvent)
-	 * afterViewChange()}) a view change occurs.
-	 *
-	 * @param listener
-	 *            Listener to invoke during a view change.
-	 */
-	public void addViewChangeListener(final ViewChangeListener listener)
-	{
-		if(this.navigator != null)
-		{
-			this.navigator.addViewChangeListener(listener);
-		}
-		else
-		{
-			if(this.viewChangeListeners == null)
-			{
-				this.viewChangeListeners = new ArrayList<>();
-			}
-			this.viewChangeListeners.add(listener);
-		}
-	}
-
-
-	/**
-	 * Removes a view change listener.
-	 *
-	 * @param listener
-	 *            Listener to remove.
-	 */
-	public void removeViewChangeListener(final ViewChangeListener listener)
-	{
-		if(this.navigator != null)
-		{
-			this.navigator.removeViewChangeListener(listener);
-		}
-		else if(this.viewChangeListeners != null)
-		{
-			this.viewChangeListeners.remove(listener);
-			if(this.viewChangeListeners.isEmpty())
-			{
-				this.viewChangeListeners = null;
-			}
-		}
-	}
-
-
-	/**
-	 * Navigates to a view and initialize the view with given parameters.
-	 * <p>
-	 * The view string consists of a view name optionally followed by a slash
-	 * and a parameters part that is passed as-is to the view. ViewProviders are
-	 * used to find and create the correct type of view.
-	 * <p>
-	 * If multiple providers return a matching view, the view with the longest
-	 * name is selected. This way, e.g. hierarchies of subviews can be
-	 * registered like "admin/", "admin/users", "admin/settings" and the longest
-	 * match is used.
-	 * <p>
-	 * If the view being deactivated indicates it wants a confirmation for the
-	 * navigation operation, the user is asked for the confirmation.
-	 * <p>
-	 * Registered {@link ViewChangeListener}s are called upon successful view
-	 * change.
-	 *
-	 * @param navigationState
-	 *            view name and parameters
-	 *
-	 * @throws IllegalArgumentException
-	 *             if <code>navigationState</code> does not map to a known view
-	 *             and no error view is registered
-	 */
-	public void navigateTo(final String navigationState)
-	{
-		if(this.navigator != null)
-		{
-			this.navigator.navigateTo(navigationState);
-		}
-		else
-		{
-			this.initialNavigationState = navigationState;
-		}
-	}
-
-
-
-	private class XdevViewProvider implements ViewProvider
-	{
-		@Override
-		public String getViewName(final String viewAndParameters)
-		{
-			for(final String path : XdevNavigator.this.views.keySet())
-			{
-				if(viewAndParameters.equals(path) || viewAndParameters.startsWith(path + "/"))
-				{
-					return path;
-				}
-			}
-
-			return null;
-		}
-
-
-		@Override
-		public View getView(final String viewName)
-		{
-			final ViewEntry entry = XdevNavigator.this.views.get(viewName);
-			if(entry != null)
-			{
-				return entry.getView();
-			}
-
-			return null;
-		}
-	}
-
-
-
-	public static interface ViewEntry
-	{
-		/**
-		 * @return the path
-		 */
-		public String getPath();
-
-
-		/**
-		 * @return the view
-		 */
-		public View getView();
-
-
-		/**
-		 * @return the title
-		 */
-		public String getTitle();
-	}
-
-
-
-	private static class XdevViewEntry implements ViewEntry
-	{
-		private final String				path;
-		private final Class<? extends View>	viewType;
-		private View						view;
-		private final String				title;
-
-
-		private XdevViewEntry(final String path, final Class<? extends View> viewType,
-				final String title)
-		{
-			this.path = path;
-			this.viewType = viewType;
-			this.title = title;
-		}
-
-
-		private <V extends View> XdevViewEntry(final String path, final V view, final String title)
-		{
-			this.path = path;
-			this.viewType = view.getClass();
-			this.view = view;
-			this.title = title;
-		}
-
-
-		@Override
-		public String getPath()
-		{
-			return this.path;
-		}
-
-
-		@Override
-		public View getView()
-		{
-			if(this.view == null)
-			{
-				try
-				{
-					this.view = this.viewType.newInstance();
-				}
-				catch(final Exception e)
-				{
-					// Checked exception is not possible, ViewProvider#getView()
-					// throws nothing.
-					throw new RuntimeException(e);
-				}
-			}
-
-			return this.view;
-		}
-
-
-		@Override
-		public String getTitle()
-		{
-			return this.title;
-		}
+		super(ui,display);
 	}
 }

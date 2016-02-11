efc1a3a763b911fcad7d0d79e0c14e0dff8caf31
FH <f.habermann@xdev-software.de>
2016-02-11 13:33:35 +0100

    Added Gson serializer/deserializer

---
+++ ui/src/com/xdev/ui/persistence/gson/GuiPersistenceGson.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2013-2016 by XDEV Software, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.xdev.ui.persistence.gson;
+
+
+import java.lang.reflect.Type;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.xdev.ui.persistence.GuiPersistenceEntry;
+import com.xdev.ui.persistence.GuiPersistentStates;
+
+
+/**
+ * @author XDEV Software
+ *
+ */
+public interface GuiPersistenceGson
+{
+	public final static GuiPersistenceGson DEFAULT = new Implementation();
+
+
+	public String serialize(final GuiPersistentStates states);
+	
+	
+	public GuiPersistentStates deserialize(final String json) throws JsonParseException;
+
+
+
+	public static class Implementation implements GuiPersistenceGson
+	{
+		@Override
+		public String serialize(final GuiPersistentStates states)
+		{
+			return gson().toJson(states,GuiPersistentStates.class);
+		}
+		
+		
+		@Override
+		public GuiPersistentStates deserialize(final String json) throws JsonParseException
+		{
+			return gson().fromJson(json,GuiPersistentStates.class);
+		}
+		
+		
+		protected Gson gson()
+		{
+			return new GsonBuilder().registerTypeAdapter(Object[].class,new ObjectArrayHandler())
+					.registerTypeAdapter(GuiPersistenceEntry.class,new GuiPersistenceEntryHandler())
+					.enableComplexMapKeySerialization().create();
+		}
+		
+		
+		
+		protected static abstract class Handler
+		{
+			public JsonElement serializeObject(final Object value,
+					final JsonSerializationContext context)
+			{
+				if(value == null)
+				{
+					return JsonNull.INSTANCE;
+				}
+				
+				final Class<?> clazz = value.getClass();
+				final JsonObject jsonObj = new JsonObject();
+				jsonObj.addProperty("class",clazz.getName());
+				jsonObj.add("value",context.serialize(value,clazz));
+				return jsonObj;
+			}
+			
+			
+			public Object deserializeObject(final JsonElement element,
+					final JsonDeserializationContext context) throws JsonParseException
+			{
+				if(element.isJsonNull())
+				{
+					return null;
+				}
+				
+				if(!element.isJsonObject())
+				{
+					throw new JsonParseException("Unexpected element: " + element);
+				}
+				
+				final JsonObject jsonObj = (JsonObject)element;
+				final String clazzName = jsonObj.get("class").getAsString();
+				try
+				{
+					final Class<?> clazz = Class.forName(clazzName);
+					return context.deserialize(jsonObj.get("value"),clazz);
+				}
+				catch(final ClassNotFoundException e)
+				{
+					throw new JsonParseException(e);
+				}
+			}
+		}
+		
+		
+		
+		protected static class GuiPersistenceEntryHandler extends Handler implements
+				JsonSerializer<GuiPersistenceEntry>, JsonDeserializer<GuiPersistenceEntry>
+		{
+			@Override
+			public JsonElement serialize(final GuiPersistenceEntry entry, final Type type,
+					final JsonSerializationContext context)
+			{
+				final JsonObject jsonObj = new JsonObject();
+				
+				entry.values().forEach((key, value) -> {
+					jsonObj.add(key,serializeObject(value,context));
+				});
+				
+				return jsonObj;
+			}
+			
+			
+			@Override
+			public GuiPersistenceEntry deserialize(final JsonElement element, final Type type,
+					final JsonDeserializationContext context) throws JsonParseException
+			{
+				final Map<String, Object> values = new LinkedHashMap<>();
+				
+				final JsonObject jsonObj = (JsonObject)element;
+				jsonObj.entrySet().forEach(entry -> {
+					final String key = entry.getKey();
+					final JsonElement jsonElement = entry.getValue();
+					values.put(key,deserializeObject(jsonElement,context));
+				});
+				
+				return GuiPersistenceEntry.New(values);
+			}
+		}
+		
+		
+		
+		protected static class ObjectArrayHandler extends Handler
+				implements JsonSerializer<Object[]>, JsonDeserializer<Object[]>
+		{
+			@Override
+			public JsonElement serialize(final Object[] array, final Type type,
+					final JsonSerializationContext context)
+			{
+				final JsonArray jsonArray = new JsonArray();
+				for(final Object value : array)
+				{
+					jsonArray.add(serializeObject(value,context));
+				}
+				return jsonArray;
+			}
+			
+			
+			@Override
+			public Object[] deserialize(final JsonElement element, final Type type,
+					final JsonDeserializationContext context) throws JsonParseException
+			{
+				final JsonArray jsonArray = (JsonArray)element;
+				final int size = jsonArray.size();
+				final Object[] array = new Object[size];
+				for(int i = 0; i < size; i++)
+				{
+					array[i] = deserializeObject(jsonArray.get(i),context);
+				}
+				return array;
+			}
+		}
+	}
+}

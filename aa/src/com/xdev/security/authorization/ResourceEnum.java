2d360c7ff478525206afa5a84f122de5b52bd884
FH <f.habermann@xdev-software.de>
2016-02-15 16:32:19 +0100

    ResourceEnum Update

--- aa/src/com/xdev/security/authorization/ResourceEnum.java
+++ aa/src/com/xdev/security/authorization/ResourceEnum.java
@@ -19,12 +19,20 @@ package com.xdev.security.authorization;
 
 
 /**
- * Marker interface for central resource collection enums.
+ * Contract for central resource collection enums.
  *
  * @author XDEV Software
  */
 public interface ResourceEnum<E extends Enum<E> & ResourceEnum<E>>
 {
+	/**
+	 * The resource's name
+	 *
+	 * @return the name of the resource
+	 */
+	public String resourceName();
+
+
 	/**
 	 * Gets the actual resource held by this enum entry.
 	 *

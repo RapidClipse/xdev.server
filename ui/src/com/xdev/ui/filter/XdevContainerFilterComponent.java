15808299419e129464226c4c7f221f1743f3894a
FH <f.habermann@xdev-software.de>
2016-02-16 21:32:34 +0100

    XWS-888

--- ui/src/com/xdev/ui/filter/XdevContainerFilterComponent.java
+++ ui/src/com/xdev/ui/filter/XdevContainerFilterComponent.java
@@ -18,11 +18,14 @@
 package com.xdev.ui.filter;
 
 
+import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.EventListener;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -34,10 +37,12 @@ import com.vaadin.data.Container.Filter;
 import com.vaadin.data.util.filter.And;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.util.ReflectTools;
 import com.xdev.res.StringResourceUtils;
 import com.xdev.ui.XdevComponent;
 import com.xdev.ui.XdevGridLayout;
@@ -54,32 +59,65 @@ import com.xdev.util.JPAMetaDataUtils;
 public class XdevContainerFilterComponent extends CustomComponent
 		implements FilterSettings, XdevComponent
 {
-	public final static String			FILTER_ADD_BUTTON_CLASS		= "x-containerfilter-add-button";
-	public final static String			FILTER_REMOVE_BUTTON_CLASS	= "x-containerfilter-remove-button";
-	public final static String			FILTER_SEARCHFIELD_CLASS	= "x-containerfilter-searchfield";
-	public final static String			FILTER_COMBOBOX_CLASS		= "x-containerfilter-combobox";
-	public final static String			FILTER_EDITOR_CLASS			= "x-containerfilter-editor";
+	public final static String	FILTER_ADD_BUTTON_CLASS		= "x-containerfilter-add-button";
+	public final static String	FILTER_REMOVE_BUTTON_CLASS	= "x-containerfilter-remove-button";
+	public final static String	FILTER_SEARCHFIELD_CLASS	= "x-containerfilter-searchfield";
+	public final static String	FILTER_COMBOBOX_CLASS		= "x-containerfilter-combobox";
+	public final static String	FILTER_EDITOR_CLASS			= "x-containerfilter-editor";
+															
+															
+															
+	public static class FilterChangeEvent extends Component.Event
+	{
+		private final Filter filter;
+		
+		
+		public FilterChangeEvent(final Component source, final Filter filter)
+		{
+			super(source);
+			this.filter = filter;
+		}
+		
+		
+		public Filter getFilter()
+		{
+			return this.filter;
+		}
+	}
 
-	private final Extensions			extensions					= new Extensions();
 
+
+	public static interface FilterChangeListener extends EventListener, Serializable
+	{
+		public static final Method FILTER_CHANGED_METHOD = ReflectTools
+				.findMethod(FilterChangeListener.class,"filterChanged",FilterChangeEvent.class);
+
+
+		public void filterChanged(FilterChangeEvent event);
+	}
+	
+	private final Extensions			extensions				= new Extensions();
+																
 	private final VerticalLayout		rootLayout;
 	private final TextFilterField		searchTextField;
 	private final Button				addFilterButton;
 	private final XdevGridLayout		filterLayout;
-
+										
 	private Container.Filterable		container;
-	private Collection<?>				filterableProperties		= Collections.emptyList();
-	private Collection<?>				searchableProperties		= Collections.emptyList();
-
-	private SearchFilterGenerator		searchFilterGenerator		= new SearchFilterGenerator.Default();
-
-	private boolean						caseSensitive				= false;
-	private char						wildcard					= '*';
-
-	private final List<FilterEditor>	filterEditors				= new ArrayList<>();
-	private boolean						layoutFilters				= true;
-
-
+	private Collection<?>				filterableProperties	= Collections.emptyList();
+	private Collection<?>				searchableProperties	= Collections.emptyList();
+																
+	private SearchFilterGenerator		searchFilterGenerator	= new SearchFilterGenerator.Default();
+																
+	private boolean						caseSensitive			= false;
+	private char						wildcard				= '*';
+																
+	private final List<FilterEditor>	filterEditors			= new ArrayList<>();
+	private boolean						layoutFilters			= true;
+																
+	private Filter						currentFilter;
+										
+										
 	/**
 	 *
 	 */
@@ -89,52 +127,52 @@ public class XdevContainerFilterComponent extends CustomComponent
 		this.searchTextField.addFilterFieldChangeListener(event -> {
 			updateContainerFilter();
 		});
-
+		
 		this.addFilterButton = createAddFilterButton();
 		this.addFilterButton.addClickListener(event -> addFilterEditor(0));
-
+		
 		final HorizontalLayout headerLayout = new HorizontalLayout(this.searchTextField,
 				this.addFilterButton);
 		headerLayout.setMargin(false);
 		headerLayout.setSpacing(true);
-
+		
 		this.searchTextField.setWidth(100,Unit.PERCENTAGE);
 		headerLayout.setExpandRatio(this.searchTextField,1f);
-
+		
 		this.filterLayout = new XdevGridLayout();
 		this.filterLayout.setMargin(false);
 		this.filterLayout.setSpacing(true);
 		this.filterLayout.setColumnExpandRatio(2,1f);
 		this.filterLayout.setVisible(false);
-
+		
 		this.rootLayout = new VerticalLayout(headerLayout,this.filterLayout);
 		this.rootLayout.setMargin(false);
 		this.rootLayout.setSpacing(true);
-
+		
 		headerLayout.setWidth(100,Unit.PERCENTAGE);
 		this.rootLayout.setExpandRatio(headerLayout,1f);
-
+		
 		this.filterLayout.setWidth(100,Unit.PERCENTAGE);
 		this.rootLayout.setExpandRatio(this.filterLayout,1f);
-
+		
 		setCompositionRoot(this.rootLayout);
 	}
-
-
+	
+	
 	protected TextFilterField createSearchTextField()
 	{
 		final TextFilterField searchTextField = new TextFilterField();
 		searchTextField.addStyleName(FILTER_SEARCHFIELD_CLASS);
 		return searchTextField;
 	}
-
-
+	
+	
 	public TextFilterField getSearchTextField()
 	{
 		return this.searchTextField;
 	}
-
-
+	
+	
 	protected Button createAddFilterButton()
 	{
 		final Button addFilterButton = new Button();
@@ -145,14 +183,14 @@ public class XdevContainerFilterComponent extends CustomComponent
 				StringResourceUtils.getResourceString("ContainerFilterComponent.addFilter",this));
 		return addFilterButton;
 	}
-
-
+	
+	
 	public Button getAddFilterButton()
 	{
 		return this.addFilterButton;
 	}
-
-
+	
+	
 	public void setContainer(final Container.Filterable container,
 			final Object... filterableProperties)
 	{
@@ -161,11 +199,11 @@ public class XdevContainerFilterComponent extends CustomComponent
 				? Arrays.asList(filterableProperties) : container.getContainerPropertyIds();
 		setSearchableProperties(this.filterableProperties.stream()
 				.filter(p -> getPropertyType(p) == String.class).toArray());
-
+				
 		reset();
 	}
-
-
+	
+	
 	/**
 	 * @return the container
 	 */
@@ -173,16 +211,16 @@ public class XdevContainerFilterComponent extends CustomComponent
 	{
 		return this.container;
 	}
-
-
+	
+	
 	public void setSearchableProperties(final Object... searchableProperties)
 	{
 		this.searchableProperties = Arrays.asList(searchableProperties);
-
+		
 		updateSearchTextFieldInputPrompt();
 	}
-
-
+	
+	
 	protected void updateSearchTextFieldInputPrompt()
 	{
 		final String res = StringResourceUtils
@@ -192,72 +230,72 @@ public class XdevContainerFilterComponent extends CustomComponent
 		final String prompt = MessageFormat.format(res,properties);
 		this.searchTextField.setInputPrompt(prompt);
 	}
-
-
+	
+	
 	@Override
 	public Object[] getFilterableProperties()
 	{
 		return this.filterableProperties.toArray();
 	}
-
-
+	
+	
 	@Override
 	public Object[] getSearchableProperties()
 	{
 		return this.searchableProperties.toArray();
 	}
-
-
+	
+	
 	public void setSearchFilterGenerator(final SearchFilterGenerator searchFilterGenerator)
 	{
 		this.searchFilterGenerator = searchFilterGenerator;
 	}
-
-
+	
+	
 	public SearchFilterGenerator getSearchFilterGenerator()
 	{
 		return this.searchFilterGenerator;
 	}
-
-
+	
+	
 	public void setFilterEnabled(final boolean enabled)
 	{
 		this.addFilterButton.setVisible(enabled);
 	}
-
-
+	
+	
 	public boolean isFilterEnabled()
 	{
 		return this.addFilterButton.isVisible();
 	}
-
-
+	
+	
 	public void setCaseSensitive(final boolean caseSensitive)
 	{
 		this.caseSensitive = caseSensitive;
 	}
-
-
+	
+	
 	@Override
 	public boolean isCaseSensitive()
 	{
 		return this.caseSensitive;
 	}
-
-
+	
+	
 	public void setWildcard(final char wildcard)
 	{
 		this.wildcard = wildcard;
 	}
-
-
+	
+	
 	@Override
 	public char getWildcard()
 	{
 		return this.wildcard;
 	}
-
-
+	
+	
 	protected void reset()
 	{
 		if(this.container != null)
@@ -268,36 +306,33 @@ public class XdevContainerFilterComponent extends CustomComponent
 		this.filterEditors.clear();
 		this.filterLayout.removeAllComponents();
 	}
-
-
+	
+	
 	protected void updateContainerFilter()
 	{
-		if(this.container == null)
+		if(this.container != null)
 		{
-			return;
-		}
-
-		final Filter newFilter = createFilter();
-		final Collection<Filter> oldFilters = this.container.getContainerFilters();
-		if(newFilter == null)
-		{
-			if(oldFilters.size() > 0)
+			final Filter newFilter = createFilter();
+			
+			if(!Objects.equals(newFilter,this.currentFilter))
 			{
-				this.container.removeAllContainerFilters();
+				if(this.currentFilter != null)
+				{
+					this.container.removeContainerFilter(this.currentFilter);
+				}
+				if(newFilter != null)
+				{
+					this.container.addContainerFilter(newFilter);
+				}
+				
+				this.currentFilter = newFilter;
+
+				fireEvent(new FilterChangeEvent(this,newFilter));
 			}
 		}
-		else if(oldFilters.isEmpty())
-		{
-			this.container.addContainerFilter(newFilter);
-		}
-		else if(oldFilters.size() > 1 || !oldFilters.iterator().next().equals(newFilter))
-		{
-			this.container.removeAllContainerFilters();
-			this.container.addContainerFilter(newFilter);
-		}
 	}
-
-
+	
+	
 	protected Filter createFilter()
 	{
 		final Filter searchFilter = createSearchFilter();
@@ -306,36 +341,36 @@ public class XdevContainerFilterComponent extends CustomComponent
 		{
 			return new And(searchFilter,valueFilter);
 		}
-		else if(searchFilter != null)
+		if(searchFilter != null)
 		{
 			return searchFilter;
 		}
-		else if(valueFilter != null)
+		if(valueFilter != null)
 		{
 			return valueFilter;
 		}
 		return null;
 	}
-
-
+	
+	
 	protected Filter createSearchFilter()
 	{
 		if(this.searchFilterGenerator != null)
 		{
 			return this.searchFilterGenerator.createSearchFilter(getSearchText(),this);
 		}
-
+		
 		return null;
 	}
-
-
+	
+	
 	protected Filter createValueFilter()
 	{
 		if(this.filterEditors.isEmpty())
 		{
 			return null;
 		}
-
+		
 		final List<Filter> valueFilters = this.filterEditors.stream()
 				.map(editor -> editor.getFilter()).filter(Objects::nonNull)
 				.collect(Collectors.toList());
@@ -343,43 +378,49 @@ public class XdevContainerFilterComponent extends CustomComponent
 		{
 			return null;
 		}
-
+		
 		final int count = valueFilters.size();
 		if(count == 1)
 		{
 			return valueFilters.get(0);
 		}
-
+		
 		return new And(valueFilters.toArray(new Filter[count]));
 	}
-
-
+	
+	
+	public Filter getFilter()
+	{
+		return this.currentFilter;
+	}
+	
+	
 	public String getSearchText()
 	{
 		return (String)this.searchTextField.getFilterValue();
 	}
-
-
+	
+	
 	public void setSearchText(final String searchText)
 	{
 		this.searchTextField.setFilterValue(searchText != null ? searchText : "");
 	}
-
-
+	
+	
 	public FilterData[] getFilterData()
 	{
 		final List<FilterData> list = this.filterEditors.stream().map(FilterEditor::getFilterData)
 				.filter(Objects::nonNull).collect(Collectors.toList());
 		return list.toArray(new FilterData[list.size()]);
 	}
-
-
+	
+	
 	public void setFilterData(final FilterData[] filterData)
 	{
 		try
 		{
 			this.layoutFilters = false;
-
+			
 			this.filterEditors.clear();
 			if(filterData != null)
 			{
@@ -395,14 +436,14 @@ public class XdevContainerFilterComponent extends CustomComponent
 			layoutFilters();
 		}
 	}
-
-
+	
+	
 	protected FilterEditor addFilterEditorAfter(final FilterEditor filterEditor)
 	{
 		return addFilterEditor(this.filterEditors.indexOf(filterEditor) + 1);
 	}
-
-
+	
+	
 	protected FilterEditor addFilterEditor(final int index)
 	{
 		final FilterEditor editor = createFilterEditor();
@@ -410,46 +451,46 @@ public class XdevContainerFilterComponent extends CustomComponent
 		layoutFilters();
 		return editor;
 	}
-
-
+	
+	
 	protected FilterEditor createFilterEditor()
 	{
 		return new FilterEditor(this);
 	}
-
-
+	
+	
 	protected void removeFilterEditor(final FilterEditor filterEditor)
 	{
 		this.filterEditors.remove(filterEditor);
 		layoutFilters();
 	}
-
-
+	
+	
 	protected FilterEditor[] getFilterEditors()
 	{
 		return this.filterEditors.toArray(new FilterEditor[this.filterEditors.size()]);
 	}
-
-
+	
+	
 	protected void layoutFilters()
 	{
 		if(!this.layoutFilters)
 		{
 			return;
 		}
-
+		
 		this.filterLayout.removeAllComponents();
 		this.filterLayout.setRows(1);
-
+		
 		int row = 0;
-
+		
 		for(final FilterEditor filterEditor : this.filterEditors)
 		{
 			this.filterLayout.addComponent(filterEditor.getPropertyComboBox(),0,row);
 			this.filterLayout.addComponent(filterEditor.getOperatorComboBox(),1,row);
 			this.filterLayout.addComponent(filterEditor.getRemoveFilterButton(),3,row);
 			this.filterLayout.addComponent(filterEditor.getAddFilterButton(),4,row);
-
+			
 			final FilterField<?>[] valueEditors = filterEditor.getValueEditors();
 			if(valueEditors != null && valueEditors.length > 0)
 			{
@@ -469,16 +510,16 @@ public class XdevContainerFilterComponent extends CustomComponent
 					this.filterLayout.addComponent(hLayout,2,row);
 				}
 			}
-
+			
 			row++;
 		}
-
+		
 		this.filterLayout.setVisible(row > 0);
-
+		
 		updateContainerFilter();
 	}
-
-
+	
+	
 	protected String getPropertyCaption(final Object propertyId)
 	{
 		final Container.Filterable container = getContainer();
@@ -487,15 +528,15 @@ public class XdevContainerFilterComponent extends CustomComponent
 			return CaptionUtils.resolveEntityMemberCaption(
 					((XdevBeanContainer<?>)container).getBeanType(),propertyId.toString());
 		}
-
+		
 		return propertyId.toString();
 	}
-
-
+	
+	
 	protected Class<?> getPropertyType(final Object propertyId)
 	{
 		final Class<?> propertyType = this.container.getType(propertyId);
-
+		
 		if(propertyType == null)
 		{
 			final Container.Filterable container = getContainer();
@@ -510,11 +551,11 @@ public class XdevContainerFilterComponent extends CustomComponent
 				}
 			}
 		}
-
+		
 		return propertyType;
 	}
-
-
+	
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -523,8 +564,8 @@ public class XdevContainerFilterComponent extends CustomComponent
 	{
 		return this.extensions.add(type,extension);
 	}
-
-
+	
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -532,5 +573,17 @@ public class XdevContainerFilterComponent extends CustomComponent
 	public <E> E getExtension(final Class<E> type)
 	{
 		return this.extensions.get(type);
+	}
+
+
+	public void addFilterChangeListener(final FilterChangeListener listener)
+	{
+		addListener(FilterChangeEvent.class,listener,FilterChangeListener.FILTER_CHANGED_METHOD);
+	}
+	
+	
+	public void removeFilterChangeListener(final FilterChangeListener listener)
+	{
+		removeListener(FilterChangeEvent.class,listener,FilterChangeListener.FILTER_CHANGED_METHOD);
 	}
 }

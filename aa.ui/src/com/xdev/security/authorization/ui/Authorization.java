adfad0b424be65b736f0fa27fc308f75b8fbcdb1
FH <f.habermann@xdev-software.de>
2016-02-22 14:57:16 +0100

    XWS-902

--- aa.ui/src/com/xdev/security/authorization/ui/Authorization.java
+++ aa.ui/src/com/xdev/security/authorization/ui/Authorization.java
@@ -33,28 +33,56 @@ import com.xdev.ui.XdevComponent;
  * Utility class for authorization purposes.
  *
  * @author XDEV Software
- *		
+ * 		
  */
 public final class Authorization
 {
 	private Authorization()
 	{
 	}
-
-
+	
+	
+	/**
+	 * Registers an {@link AuthorizationManager} in the current user session.
+	 *
+	 * @param authorizationManager
+	 *            the {@link AuthorizationManager} to register
+	 * @see #getAuthorizationManager()
+	 */
 	public static void setAuthorizationManager(final AuthorizationManager authorizationManager)
 	{
 		UI.getCurrent().getSession().setAttribute(AuthorizationManager.class,authorizationManager);
 	}
-
-
+	
+	
+	/**
+	 * Returns the {@link AuthorizationManager} of the current user session.
+	 *
+	 * @return the {@link AuthorizationManager}
+	 * @see #setAuthorizationManager(AuthorizationManager)
+	 */
 	public static AuthorizationManager getAuthorizationManager()
 	{
 		return UI.getCurrent().getSession().getAttribute(AuthorizationManager.class);
 	}
-
-
+	
+	
+	/**
+	 * Searches for a specific resource in the current
+	 * {@link AuthorizationManager}. If no resource is found an
+	 * {@link IllegalArgumentException} is thrown.
+	 *
+	 * @param name
+	 *            the resource's name to search for
+	 * @return the resource
+	 * @throws IllegalStateException
+	 *             if no authorization manager has been initialized
+	 * @throws IllegalArgumentException
+	 *             if the resources couldn't be found
+	 * @see #resource(String)
+	 */
 	public static Resource getResource(final String name)
+			throws IllegalStateException, IllegalArgumentException
 	{
 		final AuthorizationManager authorizationManager = getAuthorizationManager();
 		if(authorizationManager == null)
@@ -68,15 +96,61 @@ public final class Authorization
 		}
 		return resource;
 	}
-
-
+	
+	
+	/**
+	 * Searches for a specific resource with {@link #getResource(String)}. If no
+	 * resource with the specific name is present a new one will be created and
+	 * returned.
+	 *
+	 * @param name
+	 *            the resource's name
+	 * @return the found or created resource
+	 * @see #getResource(String)
+	 */
+	public static Resource resource(final String name)
+	{
+		try
+		{
+			return getResource(name);
+		}
+		catch(IllegalStateException | IllegalArgumentException e)
+		{
+			return Resource.New(name);
+		}
+	}
+	
+	
+	/**
+	 * Registers a {@link SubjectEvaluatingComponentExtension} with
+	 * <code>component</code>.
+	 *
+	 * @param component
+	 *            the component to register the extension with
+	 * @param extension
+	 *            the extension to register
+	 * @see #evaluateComponents(XdevComponent)
+	 */
 	public static void setSubjectEvaluatingComponentExtension(final XdevComponent component,
 			final SubjectEvaluatingComponentExtension extension)
 	{
 		component.addExtension(SubjectEvaluatingComponentExtension.class,extension);
 	}
-
-
+	
+	
+	/**
+	 * Evaluates all {@link SubjectEvaluatingComponentExtension}s in the
+	 * component hierarchy of <code>root</code> against the current user.
+	 *
+	 * @param root
+	 *            the root component
+	 * @see Authentication#isUserLoggedIn()
+	 * @see Authentication#login(Subject, Object)
+	 * @see Authentication#setUser(Subject, Object)
+	 * @see Authentication#getUser()
+	 * @see #setSubjectEvaluatingComponentExtension(XdevComponent,
+	 *      SubjectEvaluatingComponentExtension)
+	 */
 	public static void evaluateComponents(final XdevComponent root)
 	{
 		if(Authentication.isUserLoggedIn())
@@ -84,12 +158,25 @@ public final class Authorization
 			evaluateComponents(root,Authentication.getUser());
 		}
 	}
-
-
+	
+	
+	/**
+	 * Evaluates all {@link SubjectEvaluatingComponentExtension}s in the
+	 * component hierarchy of <code>root</code> against the given
+	 * <code>subject</code>.
+	 *
+	 * @param root
+	 *            the root component
+	 * @param subject
+	 *            the subject to evaluate against
+	 * @see #evaluateComponents(XdevComponent)
+	 * @see #setSubjectEvaluatingComponentExtension(XdevComponent,
+	 *      SubjectEvaluatingComponentExtension)
+	 */
 	public static void evaluateComponents(final XdevComponent root, final Subject subject)
 	{
 		evaluateComponent(root,subject);
-
+		
 		Iterable<?> children = null;
 		if(root instanceof HasComponents)
 		{
@@ -114,8 +201,22 @@ public final class Authorization
 			}
 		}
 	}
-
-
+	
+	
+	/**
+	 * Evaluates the {@link SubjectEvaluatingComponentExtension} of the
+	 * <code>component</code> against the given <code>subject</code>.
+	 * <p>
+	 * If no {@link SubjectEvaluatingComponentExtension} has been registered
+	 * with the <code>component</code> this method has no effect.
+	 *
+	 * @param component
+	 *            the component to check
+	 * @param subject
+	 *            the subject to evaluate against
+	 * @see #setSubjectEvaluatingComponentExtension(XdevComponent,
+	 *      SubjectEvaluatingComponentExtension)
+	 */
 	public static void evaluateComponent(final XdevComponent component, final Subject subject)
 	{
 		final SubjectEvaluatingComponentExtension extension = component

3b31547b0ef2d1b1964d1bf3ceed4e677c5858c9
FH <f.habermann@xdev-software.de>
2016-02-23 16:54:02 +0100

    SQLAuthorizationConfigurationProvider Update

--- aa/src/com/xdev/security/authorization/sql/SQLAuthorizationConfigurationProvider.java
+++ aa/src/com/xdev/security/authorization/sql/SQLAuthorizationConfigurationProvider.java
@@ -18,11 +18,12 @@
 package com.xdev.security.authorization.sql;
 
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Query;
 
@@ -44,116 +45,79 @@ public class SQLAuthorizationConfigurationProvider implements AuthorizationConfi
 	public static final AuthorizationConfiguration build(final String usersAndGroupsSelect,
 			final String rolesAndPermissionsSelect)
 	{
-		final Map<String, Set<String>> resourceResources = new HashMap<String, Set<String>>();
-		final Map<String, Set<String>> roleRoles = new HashMap<String, Set<String>>();
-		final HashMap<String, HashMap<String, Integer>> rolePermissions = new HashMap<String, HashMap<String, Integer>>();
-		final Map<String, Set<String>> subjectRoles = new HashMap<String, Set<String>>();
-		
+		final Map<String, Set<String>> resourceResources = new HashMap<>();
+		final Map<String, Set<String>> roleRoles = new HashMap<>();
+		final Map<String, Map<String, Integer>> rolePermissions = new HashMap<>();
+		final Map<String, Set<String>> subjectRoles = new HashMap<>();
+
 		final Query usersAndGroupsQuery = EntityManagerUtils.getEntityManager()
 				.createNativeQuery(usersAndGroupsSelect);
 		final List<Object[]> usersAndGroupsList = usersAndGroupsQuery.getResultList();
-		
-		for(final Object[] row : usersAndGroupsList)
-		{
-			final String user = String.valueOf(row[0]);
-			if(!subjectRoles.containsKey(user))
-			{
-				subjectRoles.put(user,unboxGroups(user,usersAndGroupsList));
-			}
-		}
-		
+
+		usersAndGroupsList.stream().map(row -> String.valueOf(row[0]))
+				.filter(user -> !subjectRoles.containsKey(user))
+				.forEach(user -> subjectRoles.put(user,unboxGroups(user,usersAndGroupsList)));
+
 		final Query rolesAndPermissionsQuery = EntityManagerUtils.getEntityManager()
 				.createNativeQuery(rolesAndPermissionsSelect);
 		final List<Object[]> rolesAndPermissionsList = rolesAndPermissionsQuery.getResultList();
-		
-		for(final Object[] row : rolesAndPermissionsList)
-		{
-			final String role = String.valueOf(row[0]);
-			if(!rolePermissions.containsKey(role))
-			{
-				rolePermissions.put(role,unboxPermissions(role,rolesAndPermissionsList));
-			}
-		}
-		
-		for(final String role : rolePermissions.keySet())
-		{
-			final Set<String> childSet = new HashSet<String>();
-			childSet.add(role);
-			
-			roleRoles.put(role,childSet);
-		}
-		
-		for(final HashMap<String, Integer> rolePermission : rolePermissions.values())
-		{
-			for(final String permission : rolePermission.keySet())
-			{
-				if(!resourceResources.containsKey(permission))
-				{
-					final Set<String> childSet = new HashSet<String>();
-					childSet.add(permission);
-					resourceResources.put(permission,childSet);
-				}
-			}
-		}
+
+		rolesAndPermissionsList.stream().map(row -> String.valueOf(row[0]))
+				.filter(role -> !rolePermissions.containsKey(role)).forEach(role -> rolePermissions
+						.put(role,unboxPermissions(role,rolesAndPermissionsList)));
+
+		rolePermissions.forEach((role, permissions) -> {
+			roleRoles.put(role,Collections.EMPTY_SET);
+			permissions.keySet()
+					.forEach(permission -> resourceResources.put(permission,Collections.EMPTY_SET));
+		});
+
+		subjectRoles.values()
+				.forEach(roles -> roles.forEach(role -> roleRoles.put(role,Collections.EMPTY_SET)));
+
 		return AuthorizationConfiguration.New(resourceResources,roleRoles,rolePermissions,
 				subjectRoles);
 	}
-	
-	
+
+
 	private static Set<String> unboxGroups(final String username, final List<Object[]> rows)
 	{
-		final Set<String> groups = new HashSet<String>();
-		
-		for(final Object[] row : rows)
-		{
-			if(username.equals(row[0]))
-			{
-				groups.add((String)row[1]);
-			}
-		}
-		
-		return groups;
+		return rows.stream().filter(row -> username.equals(row[0])).map(row -> (String)row[1])
+				.collect(Collectors.toSet());
 	}
-	
-	
-	private static HashMap<String, Integer> unboxPermissions(final String groupName,
+
+
+	private static Map<String, Integer> unboxPermissions(final String groupName,
 			final List<Object[]> rows)
 	{
-		final HashMap<String, Integer> permissions = new HashMap<String, Integer>();
-		for(final Object[] row : rows)
-		{
-			if(groupName.equals(row[0]))
-			{
-				permissions.put((String)row[1],1);
-			}
-		}
-		return permissions;
+		return rows.stream().filter(row -> groupName.equals(row[0])).map(row -> (String)row[1])
+				.collect(Collectors.toMap(permission -> permission,permission -> 1));
 	}
-	
+
 	///////////////////////////////////////////////////////////////////////////
 	// instance fields //
 	////////////////////
-	
+
 	private final String	usersAndGroupsSelect;
 	private final String	rolesAndPermissionsSelect;
-							
-							
+
+
 	///////////////////////////////////////////////////////////////////////////
 	// constructors //
 	/////////////////
-	
+
 	public SQLAuthorizationConfigurationProvider(final String usersAndGroupsSelect,
 			final String rolesAndPermissionsSelect)
 	{
 		this.usersAndGroupsSelect = usersAndGroupsSelect;
 		this.rolesAndPermissionsSelect = rolesAndPermissionsSelect;
 	}
-	
-	
+
+
 	///////////////////////////////////////////////////////////////////////////
 	// override methods //
 	/////////////////////
-	
+
 	/**
 	 * {@inheritDoc}
 	 */

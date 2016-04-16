cdc8ccb29b1956013a2be91d8118882596eff969
Alexander Grasmück <a.grasmueck@xdev-software.de>
2016-04-16 13:44:29 +0200

    Standard Plugins eingebunden, Contacte anlegen, App beenden

--- mobilekit/WebContent/VAADIN/cordova/android/plugins/cordova-plugin-contacts/www/ContactFindOptions.js
+++ mobilekit/WebContent/VAADIN/cordova/android/plugins/cordova-plugin-contacts/www/ContactFindOptions.js
@@ -1,1 +1,1 @@
-cordova.define("cordova-plugin-contacts.ContactFindOptions",function(i,t,o){var n=function(i,t,o){this.filter=i||"",this.multiple=t!==void 0?t:!1,this.desiredFields=o!==void 0?o:[]};o.exports=n})
+cordova.define("cordova-plugin-contacts.ContactFindOptions",function(i,o,t){var n=function(i,o,t,n){this.filter=i||"",this.multiple=o!==void 0?o:!1,this.desiredFields=t!==void 0?t:[],this.hasPhoneNumber=n!==void 0?n:!1};t.exports=n})

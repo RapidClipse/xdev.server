73fa2bf6ffb3ed95eb588c0c704318644cfc42c3
FH <f.habermann@xdev-software.de>
2016-10-25 16:26:44 +0200

    XWS-1002

--- ui/src/com/xdev/ui/persistence/handler/TableHandler.java
+++ ui/src/com/xdev/ui/persistence/handler/TableHandler.java
@@ -23,6 +23,8 @@ package com.xdev.ui.persistence.handler;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import com.vaadin.ui.Table;
 import com.xdev.ui.persistence.GuiPersistenceEntry;
 
@@ -34,27 +36,27 @@ public class TableHandler extends AbstractFieldHandler<Table>
 	protected static final String	KEY_IS_COLLAPSED				= "isCollapsed";
 	protected static final String	KEY_IS_ASCENDING				= "isAscending";
 	protected static final String	KEY_COLUMN_WIDTH				= "columnWidth";
-																	
-																	
+
+
 	@Override
 	public Class<Table> handledType()
 	{
 		return Table.class;
 	}
-	
-	
+
+
 	@Override
 	protected void addEntryValues(final Map<String, Object> entryValues, final Table component)
 	{
 		super.addEntryValues(entryValues,component);
-		
+
 		entryValues.put(KEY_SORT_CONTAINER_PROPERTY_ID,component.getSortContainerPropertyId());
-		
+
 		final boolean[] comVisCol = new boolean[component.getVisibleColumns().length];
 		final int[] widthCol = new int[component.getVisibleColumns().length];
 		final Object[] allColumns = component.getVisibleColumns();
 		int i = 0;
-		
+
 		for(final Object o : allColumns)
 		{
 			widthCol[i] = component.getColumnWidth(allColumns[i]);
@@ -68,40 +70,49 @@ public class TableHandler extends AbstractFieldHandler<Table>
 			}
 			i++;
 		}
-		
+
 		entryValues.put(KEY_VISIBLE_COLUMNS,allColumns);
 		entryValues.put(KEY_IS_COLLAPSED,comVisCol);
 		entryValues.put(KEY_COLUMN_WIDTH,widthCol);
 		entryValues.put(KEY_IS_ASCENDING,component.isSortAscending());
 	}
-	
-	
+
+
 	@Override
 	public void restore(final Table component, final GuiPersistenceEntry entry)
 	{
 		super.restore(component,entry);
-		
+
 		component.setSortContainerPropertyId(entry.value(KEY_SORT_CONTAINER_PROPERTY_ID));
-		
+
 		final boolean[] comVisCol = (boolean[])entry.value(KEY_IS_COLLAPSED);
 		final int[] widthCol = (int[])entry.value(KEY_COLUMN_WIDTH);
 		final Object[] namesCol = (Object[])entry.value(KEY_VISIBLE_COLUMNS);
-		
-		if(component.isColumnCollapsingAllowed())
+
+		try
 		{
-			for(int i = 0; i < comVisCol.length; i++)
+			component.setVisibleColumns(namesCol);
+
+			if(component.isColumnCollapsingAllowed())
 			{
-				component.setColumnCollapsed(namesCol[i],comVisCol[i]);
-				component.setColumnWidth(namesCol[i],widthCol[i]);
+				for(int i = 0; i < comVisCol.length; i++)
+				{
+					component.setColumnCollapsed(namesCol[i],comVisCol[i]);
+					component.setColumnWidth(namesCol[i],widthCol[i]);
+				}
 			}
+			else
+			{
+				for(int i = 0; i < comVisCol.length; i++)
+				{
+					component.setColumnWidth(namesCol[i],widthCol[i]);
+				}
+			}
+			component.setSortAscending((boolean)entry.value(KEY_IS_ASCENDING));
 		}
-		else
+		catch(final IllegalArgumentException e)
 		{
-			for(int i = 0; i < comVisCol.length; i++)
-			{
-				component.setColumnWidth(namesCol[i],widthCol[i]);
-			}
+			Logger.getLogger(getClass()).warn(e.getMessage(),e);
 		}
-		component.setSortAscending((boolean)entry.value(KEY_IS_ASCENDING));
 	}
 }

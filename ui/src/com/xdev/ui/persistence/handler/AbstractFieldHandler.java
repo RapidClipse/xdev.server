5c7c4d3d07d37108f932a8480b139c570f082cbf
FH <f.habermann@xdev-software.de>
2016-12-20 14:51:03 +0100

    GuiPersistence Entity Handling Fix

--- ui/src/com/xdev/ui/persistence/handler/AbstractFieldHandler.java
+++ ui/src/com/xdev/ui/persistence/handler/AbstractFieldHandler.java
@@ -21,12 +21,21 @@
 package com.xdev.ui.persistence.handler;
 
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+
+import javax.persistence.metamodel.Attribute;
 
 import com.vaadin.ui.AbstractField;
 import com.vaadin.ui.Component;
+import com.xdev.dal.DAOs;
 import com.xdev.ui.XdevField;
 import com.xdev.ui.persistence.GuiPersistenceEntry;
+import com.xdev.util.JPAMetaDataUtils;
+import com.xdev.util.ReflectionUtils;
 
 
 @SuppressWarnings("rawtypes")
@@ -47,6 +56,64 @@ public abstract class AbstractFieldHandler<C extends AbstractField>
 	}
 
 
+
+	public final static class IdEntry
+	{
+		private Class<?>	entityType;
+		private Class<?>	idType;
+		private Object		id;
+
+
+		public IdEntry()
+		{
+		}
+
+
+		public IdEntry(final Class<?> entityType, final Class<?> idType, final Object id)
+		{
+			this.entityType = entityType;
+			this.idType = idType;
+			this.id = id;
+		}
+
+
+		public Class<?> getEntityType()
+		{
+			return this.entityType;
+		}
+
+
+		public void setEntityType(final Class<?> entityType)
+		{
+			this.entityType = entityType;
+		}
+
+
+		public Class<?> getIdType()
+		{
+			return this.idType;
+		}
+
+
+		public void setIdType(final Class<?> idType)
+		{
+			this.idType = idType;
+		}
+
+
+		public Object getId()
+		{
+			return this.id;
+		}
+
+
+		public void setId(final Object id)
+		{
+			this.id = id;
+		}
+	}
+
+
 	@Override
 	protected void addEntryValues(final Map<String, Object> entryValues, final C component)
 	{
@@ -54,8 +121,40 @@ public abstract class AbstractFieldHandler<C extends AbstractField>
 
 		if(persistFieldValue(component))
 		{
-			entryValues.put(KEY_VALUE,component.getValue());
+			entryValues.put(KEY_VALUE,getFieldValueToStore(component.getValue()));
 		}
+	}
+
+
+	protected Object getFieldValueToStore(final Object value)
+	{
+		if(value == null)
+		{
+			return null;
+		}
+
+		if(value instanceof Collection<?>)
+		{
+			final List<Object> list = new ArrayList<>();
+			for(final Object element : (Collection<?>)value)
+			{
+				list.add(getFieldValueToStore(element));
+			}
+			return list;
+		}
+
+		final Class<? extends Object> clazz = value.getClass();
+		if(JPAMetaDataUtils.isManaged(clazz))
+		{
+			final Attribute<?, ?> idAttribute = JPAMetaDataUtils.getIdAttribute(clazz);
+			if(idAttribute != null)
+			{
+				final Object id = ReflectionUtils.getMemberValue(value,idAttribute.getJavaMember());
+				return new IdEntry(clazz,id.getClass(),id);
+			}
+		}
+
+		return value;
 	}
 
 
@@ -67,7 +166,44 @@ public abstract class AbstractFieldHandler<C extends AbstractField>
 
 		if(persistFieldValue(component))
 		{
-			component.setValue(entry.value(KEY_VALUE));
+			component.setValue(getFieldValueToRestore(entry.value(KEY_VALUE)));
 		}
+	}
+
+
+	protected Object getFieldValueToRestore(final Object value)
+	{
+		if(value instanceof IdEntry)
+		{
+			final IdEntry idEntry = (IdEntry)value;
+			Object id = idEntry.id;
+			if(!idEntry.idType.isAssignableFrom(id.getClass()) && id instanceof Number)
+			{
+				// Correct json number type mismatch
+				if(idEntry.idType == Integer.class)
+				{
+					id = ((Number)id).intValue();
+				}
+				else if(idEntry.idType == Short.class)
+				{
+					id = ((Number)id).shortValue();
+				}
+				else if(idEntry.idType == Byte.class)
+				{
+					id = ((Number)id).byteValue();
+				}
+				else if(idEntry.idType == Long.class)
+				{
+					id = ((Number)id).longValue();
+				}
+				else if(idEntry.idType == Float.class)
+				{
+					id = ((Number)id).floatValue();
+				}
+			}
+			return DAOs.getByEntityType(idEntry.entityType).find((Serializable)id);
+		}
+
+		return value;
 	}
 }

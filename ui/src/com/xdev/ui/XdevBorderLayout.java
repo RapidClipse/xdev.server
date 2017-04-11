60fd035c503723d0302e0e3ef85a64df19912da2
FH <f.habermann@xdev-software.de>
2017-04-11 12:55:50 +0200

    XWS-1216

--- ui/src/com/xdev/ui/XdevBorderLayout.java
+++ ui/src/com/xdev/ui/XdevBorderLayout.java
@@ -66,9 +66,12 @@ import com.vaadin.ui.Component;
  * +---------------------------------------+
  * </pre>
  *
+ * @deprecated use {@link XdevGridLayout} instead
+ *
  * @author XDEV Software
  *
  */
+@Deprecated
 @SuppressWarnings("serial")
 public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 {
@@ -78,30 +81,30 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 		 * The center layout constraint (middle of container).
 		 */
 		CENTER,
-
+		
 		/**
 		 * The north layout constraint (top of container).
 		 */
 		NORTH,
-
+		
 		/**
 		 * The south layout constraint (bottom of container).
 		 */
 		SOUTH,
-
+		
 		/**
 		 * The east layout constraint (right side of container).
 		 */
 		EAST,
-
+		
 		/**
 		 * The west layout constraint (left side of container).
 		 */
 		WEST
 	}
-
-
-
+	
+	
+	
 	/**
 	 * Mode enumeration for the border layout behavior as it's described in
 	 * {@link XdevBorderLayout}.
@@ -109,30 +112,30 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 	public static enum Mode
 	{
 		NORTH_SOUTH_FULL_WIDTH,
-
+		
 		WEST_EAST_FULL_HEIGHT
 	}
-
+	
 	private final Extensions					extensions	= new Extensions();
 	private final Map<Component, Constraint>	components	= new HashMap<>();
 	private Mode								mode		= Mode.NORTH_SOUTH_FULL_WIDTH;
-	
-	
+
+
 	/**
 	 * Creates a new empty border layout.
 	 */
 	public XdevBorderLayout()
 	{
 		super(3,3);
-
+		
 		setColumnExpandRatio(1,1f);
 		setRowExpandRatio(1,1f);
 		setHideEmptyRowsAndColumns(true);
 		setMargin(true);
 		setSpacing(true);
 	}
-
-
+	
+	
 	/**
 	 * @return the mode
 	 */
@@ -140,8 +143,8 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 	{
 		return this.mode;
 	}
-
-
+	
+	
 	/**
 	 * @param mode
 	 *            the mode to set
@@ -149,14 +152,14 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 	public void setMode(final Mode mode)
 	{
 		this.mode = mode;
-
+		
 		if(getComponentCount() > 0)
 		{
 			rebuild();
 		}
 	}
-
-
+	
+	
 	protected void rebuild()
 	{
 		final Map<Component, Constraint> components = new HashMap<>(this.components);
@@ -166,12 +169,12 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 			addComponent(component,components.get(component));
 		}
 	}
-
-
+	
+	
 	public void addComponent(final Component component, final Constraint constraint)
 	{
 		this.components.put(component,constraint);
-
+		
 		switch(constraint)
 		{
 			case CENTER:
@@ -180,7 +183,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 				component.setSizeFull();
 			}
 			break;
-			
+		
 			case NORTH:
 			{
 				switch(this.mode)
@@ -188,7 +191,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 					case NORTH_SOUTH_FULL_WIDTH:
 						addComponent(component,0,0,2,0);
 					break;
-					
+				
 					case WEST_EAST_FULL_HEIGHT:
 						addComponent(component,1,0);
 					break;
@@ -196,7 +199,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 				component.setWidth(100,Unit.PERCENTAGE);
 			}
 			break;
-			
+		
 			case SOUTH:
 			{
 				switch(this.mode)
@@ -204,7 +207,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 					case NORTH_SOUTH_FULL_WIDTH:
 						addComponent(component,0,2,2,2);
 					break;
-					
+				
 					case WEST_EAST_FULL_HEIGHT:
 						addComponent(component,1,2);
 					break;
@@ -212,7 +215,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 				component.setWidth(100,Unit.PERCENTAGE);
 			}
 			break;
-			
+		
 			case WEST:
 			{
 				switch(this.mode)
@@ -220,7 +223,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 					case NORTH_SOUTH_FULL_WIDTH:
 						addComponent(component,0,1);
 					break;
-					
+				
 					case WEST_EAST_FULL_HEIGHT:
 						addComponent(component,0,0,0,2);
 					break;
@@ -228,7 +231,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 				component.setHeight(100,Unit.PERCENTAGE);
 			}
 			break;
-			
+		
 			case EAST:
 			{
 				switch(this.mode)
@@ -236,7 +239,7 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 					case NORTH_SOUTH_FULL_WIDTH:
 						addComponent(component,2,1);
 					break;
-					
+				
 					case WEST_EAST_FULL_HEIGHT:
 						addComponent(component,2,0,2,2);
 					break;
@@ -246,17 +249,17 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 			break;
 		}
 	}
-
-
+	
+	
 	@Override
 	public void removeComponent(final Component component)
 	{
 		super.removeComponent(component);
-
+		
 		this.components.remove(component);
 	}
-	
-	
+
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -265,8 +268,8 @@ public class XdevBorderLayout extends XdevGridLayout implements XdevComponent
 	{
 		return this.extensions.add(type,extension);
 	}
-	
-	
+
+
 	/**
 	 * {@inheritDoc}
 	 */

e2a0e66c5bde242ffbdb2934240eaac77d753bd8
FH <f.habermann@xdev-software.de>
2017-04-12 17:26:33 +0200

    XWS-1289

--- ui/src/com/xdev/ui/filter/FilterData.java
+++ ui/src/com/xdev/ui/filter/FilterData.java
@@ -24,6 +24,8 @@ package com.xdev.ui.filter;
 import java.io.Serializable;
 import java.util.Arrays;
 
+import org.apache.commons.lang3.ClassUtils;
+
 import com.xdev.dal.DAOs;
 import com.xdev.util.EntityIDResolver;
 import com.xdev.util.JPAEntityIDResolver;
@@ -32,7 +34,7 @@ import com.xdev.util.JPAMetaDataUtils;
 
 /**
  * @author XDEV Software
- *		
+ *
  */
 public class FilterData
 {
@@ -40,70 +42,70 @@ public class FilterData
 	{
 		private final Class<?>	entityType;
 		private final Object	id;
-								
-								
+		
+		
 		public EntityID(final Class<?> entityType, final Object id)
 		{
 			this.entityType = entityType;
 			this.id = id;
 		}
-		
-		
+
+
 		public Class<?> getEntityType()
 		{
 			return this.entityType;
 		}
-		
-		
+
+
 		public Object getId()
 		{
 			return this.id;
 		}
 	}
-	
+
 	private Object		propertyId;
 	private String		operatorKey;
 	private Object[]	values;
-						
-						
+	
+	
 	public FilterData()
 	{
 	}
-	
-	
+
+
 	public FilterData(final Object propertyId, final FilterOperator operator, final Object[] values)
 	{
 		setPropertyId(propertyId);
 		setOperator(operator);
 		setValues(values);
 	}
-	
-	
+
+
 	public void setPropertyId(final Object propertyId)
 	{
 		this.propertyId = propertyId;
 	}
-	
-	
+
+
 	public Object getPropertyId()
 	{
 		return this.propertyId;
 	}
-	
-	
+
+
 	public void setOperator(final FilterOperator operator)
 	{
 		this.operatorKey = operator.getKey();
 	}
-	
-	
+
+
 	public FilterOperator getOperator()
 	{
 		return FilterOperatorRegistry.getFilterOperators().stream()
 				.filter(op -> op.getKey().equals(this.operatorKey)).findAny().orElse(null);
 	}
-	
-	
+
+
 	public void setValues(final Object[] values)
 	{
 		if(values == null)
@@ -117,45 +119,80 @@ public class FilterData
 					.toArray();
 		}
 	}
-	
-	
+
+
 	private Object storeFilterValue(final Object value, final EntityIDResolver idResolver)
 	{
 		if(value == null)
 		{
 			return null;
 		}
-		
+
 		final Class<? extends Object> entityType = value.getClass();
 		if(JPAMetaDataUtils.isManaged(entityType))
 		{
 			return new EntityID(entityType,idResolver.getEntityIDAttributeValue(value));
 		}
-		
+
 		return value;
 	}
-	
-	
+
+
 	public Object[] getValues()
 	{
 		if(this.values == null)
 		{
 			return null;
 		}
-		
+
 		return Arrays.stream(this.values).map(this::resolveFilterValue).toArray();
 	}
-	
-	
+
+
 	protected Object resolveFilterValue(final Object filterValue)
 	{
 		if(filterValue instanceof EntityID)
 		{
 			final EntityID entityID = (EntityID)filterValue;
-			return DAOs.getByEntityType(entityID.getEntityType())
-					.find((Serializable)entityID.getId());
+			Serializable id = (Serializable)entityID.getId();
+			Class<?> idType = JPAMetaDataUtils.getIdAttribute(entityID.getEntityType())
+					.getJavaType();
+			// XXX see XWS-1289
+			if(idType != null && idType.isPrimitive())
+			{
+				idType = ClassUtils.primitiveToWrapper(idType);
+			}
+			if(idType != null && Number.class.isAssignableFrom(idType) && id instanceof Number
+					&& !idType.isInstance(id))
+			{
+				if(Integer.class.isAssignableFrom(idType))
+				{
+					id = ((Number)id).intValue();
+				}
+				else if(Long.class.isAssignableFrom(idType))
+				{
+					id = ((Number)id).longValue();
+				}
+				else if(Short.class.isAssignableFrom(idType))
+				{
+					id = ((Number)id).shortValue();
+				}
+				else if(Byte.class.isAssignableFrom(idType))
+				{
+					id = ((Number)id).byteValue();
+				}
+				else if(Double.class.isAssignableFrom(idType))
+				{
+					id = ((Number)id).doubleValue();
+				}
+				else if(Float.class.isAssignableFrom(idType))
+				{
+					id = ((Number)id).floatValue();
+				}
+			}
+			return DAOs.getByEntityType(entityID.getEntityType()).find(id);
 		}
-		
+
 		return filterValue;
 	}
 }

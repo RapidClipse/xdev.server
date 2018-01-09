5e08be03c1596d6df02db00a8bdf3bb6ebfbfb05
FH <f.habermann@xdev-software.de>
2018-01-09 14:03:29 +0100

    Rebuild

--- ui/src/com/xdev/ui/XdevSelect.java
+++ ui/src/com/xdev/ui/XdevSelect.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2017 by XDEV Software, All Rights Reserved.
+ * Copyright (C) 2013-2018 by XDEV Software, All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as

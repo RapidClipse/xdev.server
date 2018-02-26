e8cf70edbc211c55c3d3a20a0e9ffd6524d19498
FH <f.habermann@xdev-software.de>
2018-02-26 10:19:13 +0100

    XWS-1502

--- reports/src/com/xdev/reports/tableexport/ui/TableExportPopup.java
+++ reports/src/com/xdev/reports/tableexport/ui/TableExportPopup.java
@@ -68,14 +68,14 @@ public class TableExportPopup
 	{
 		show(table,ExportType.values());
 	}
-	
-	
+
+
 	public static void show(final XdevTable<?> table, final ExportType... availableExportTypes)
 	{
 		PopupWindow.For(new TableExportPopup(table,availableExportTypes).getRoot()).closable(true)
 				.draggable(true).resizable(false).modal(true).show();
 	}
-	
+
 	protected final String				PROPERTY_SELECTED		= "...";
 	protected final String				PROPERTY_COLUMNNAME		= StringResourceUtils
 			.localizeString("{$columnName}",this);
@@ -83,94 +83,94 @@ public class TableExportPopup
 			.localizeString("{$columnWidth}",this);
 	protected final String				PROPERTY_COLUMNID		= StringResourceUtils
 			.localizeString("{$columnId}",this);
-	
+
 	protected TableReportBuilder		tableReportBuilder		= TableReportBuilder.DEFAULT;
 	protected final TableExportSettings	exportSettings			= new TableExportSettings();
 	protected final XdevTable<?>		table;
-	
-	
+
+
 	protected TableExportPopup(final XdevTable<?> table, final ExportType... availableExportTypes)
 	{
 		this.table = table;
-		
+
 		this.initUI();
-		
+
 		this.cbPageType.addItems((Object[])PageType.values());
 		this.cbPageType.setValue(this.exportSettings.getPageType());
-		
+
 		this.cbPageOrientation.addItems((Object[])PageOrientation.values());
 		this.cbPageOrientation.setValue(this.exportSettings.getPageOrientation());
-		
+
 		this.chkShowPageNumber.setValue(this.exportSettings.isShowPageNumber());
 		this.chkHighlightRows.setValue(this.exportSettings.isHighlightRows());
-		
+
 		this.cmbFormat.addItems((Object[])availableExportTypes);
 		this.cmbFormat.setValue(availableExportTypes[0]);
-		
+
 		this.tblColumnChooser.addContainerProperty(this.PROPERTY_SELECTED,CheckBox.class,null,"",
 				FontAwesome.EYE,Align.CENTER);
 		this.tblColumnChooser.addContainerProperty(this.PROPERTY_COLUMNNAME,String.class,null);
 		this.tblColumnChooser.addContainerProperty(this.PROPERTY_COLUMNWIDTH,TextField.class,null);
 		this.tblColumnChooser.addContainerProperty(this.PROPERTY_COLUMNID,Object.class,null);
-		
+
 		this.tblColumnChooser.setColumnExpandRatio(this.PROPERTY_COLUMNNAME,1.0F);
-		
+
 		this.tblColumnChooser.setColumnAlignment(this.PROPERTY_SELECTED,Table.Align.CENTER);
 		this.tblColumnChooser.setColumnAlignment(this.PROPERTY_COLUMNWIDTH,Table.Align.CENTER);
-		
+
 		int id = 0;
-		
+
 		for(final Object columnId : this.table.getVisibleColumns())
 		{
 			final TextField txtWidth = new TextField();
 			txtWidth.addStyleName("small, borderless");
 			txtWidth.setColumns(4);
 			txtWidth.setImmediate(true);
-			
+
 			final CheckBox selectBox = new CheckBox();
 			selectBox.setValue(true);
-			
+
 			final int columnWidth = this.table.getColumnWidth(columnId);
 			if(columnWidth > 0)
 			{
 				txtWidth.setValue(String.valueOf(columnWidth));
 			}
-			
+
 			this.tblColumnChooser.addItem(
 					new Object[]{selectBox,this.table.getColumnHeader(columnId),txtWidth,columnId},
 					++id);
-			
+
 			selectBox.addValueChangeListener(event -> check());
 			txtWidth.addValueChangeListener(event -> check());
 		}
-
+		
 		this.tblColumnChooser.setVisibleColumns(this.PROPERTY_SELECTED,this.PROPERTY_COLUMNNAME,
 				this.PROPERTY_COLUMNWIDTH);
-		
+
 		DynamicFileDownloader.install(this.cmdSave,this::createReportResource);
-		
+
 		check();
 	}
-	
-	
+
+
 	public void setReportBuilder(final TableReportBuilder reportBuilder)
 	{
 		this.tableReportBuilder = reportBuilder;
 	}
-	
-	
+
+
 	protected void cmdSelectAll_buttonClick(final Button.ClickEvent event)
 	{
 		selectAll(true);
 	}
-	
-	
+
+
 	protected void cmdSelectNone_buttonClick(final Button.ClickEvent event)
 	{
 		selectAll(false);
 	}
-	
-	
+
+
 	protected void selectAll(final boolean status)
 	{
 		for(final Object object : this.tblColumnChooser.getItemIds())
@@ -179,24 +179,24 @@ public class TableExportPopup
 			((CheckBox)item.getItemProperty(this.PROPERTY_SELECTED).getValue()).setValue(status);
 		}
 	}
-	
-	
+
+
 	protected void cbPageType_valueChange(final Property.ValueChangeEvent event)
 	{
 		this.exportSettings.setPageType((PageType)this.cbPageType.getValue());
-		
+
 		check();
 	}
-	
-	
+
+
 	protected void cbPageOrientation_valueChange(final Property.ValueChangeEvent event)
 	{
 		this.exportSettings.setPageOrientation((PageOrientation)this.cbPageOrientation.getValue());
-		
+
 		check();
 	}
-	
-	
+
+
 	protected void check()
 	{
 		if(checkColumnWidth())
@@ -212,12 +212,12 @@ public class TableExportPopup
 			this.cmdShow.setEnabled(false);
 		}
 	}
-	
-	
+
+
 	protected boolean checkColumnWidth()
 	{
 		final Integer calculateFixedWidth = calculateFixedWidth();
-		
+
 		if(this.cbPageType.getValue() != null && this.cbPageOrientation.getValue() != null)
 		{
 			if(this.cbPageOrientation.getValue().equals(PageOrientation.PORTRAIT))
@@ -235,15 +235,15 @@ public class TableExportPopup
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
-	
+
+
 	protected int calculateFixedWidth()
 	{
 		int width = 0;
-		
+
 		for(final Object object : getSelectedColumns())
 		{
 			final Item item = this.tblColumnChooser.getItem(object);
@@ -257,15 +257,15 @@ public class TableExportPopup
 			{
 			}
 		}
-		
+
 		return width;
 	}
-	
-	
+
+
 	protected List<Object> getSelectedColumns()
 	{
 		final List<Object> columnsToExport = new ArrayList<>();
-		
+
 		for(final Object object : this.tblColumnChooser.getItemIds())
 		{
 			final Item item = this.tblColumnChooser.getItem(object);
@@ -274,17 +274,17 @@ public class TableExportPopup
 				columnsToExport.add(object);
 			}
 		}
-		
+
 		return columnsToExport;
 	}
-	
-	
+
+
 	private void cmdClose_buttonClick(final Button.ClickEvent event)
 	{
 		UIUtils.getNextParent(this.root,Window.class).close();
 	}
-	
-	
+
+
 	private void cmdShow_buttonClick(final Button.ClickEvent event)
 	{
 		final BrowserFrame browserFrame = new BrowserFrame();
@@ -292,34 +292,34 @@ public class TableExportPopup
 		PopupWindow.For(browserFrame).size(400,300).caption(this.exportSettings.getTitle())
 				.maximized(true).closable(true).draggable(true).resizable(true).modal(true).show();
 	}
-	
-	
+
+
 	private Resource createReportResource() throws ReportException
 	{
 		this.exportSettings.setTitle(this.txtReportName.getValue());
 		this.exportSettings.setShowPageNumber(this.chkShowPageNumber.getValue());
 		this.exportSettings.setHighlightRows(this.chkHighlightRows.getValue());
-		
+
 		final JasperReportBuilder builder = this.tableReportBuilder.buildReport(this.table,
 				createColumns(),this.exportSettings);
 		return ((ExportType)this.cmbFormat.getValue()).exportToResource(builder,
 				this.exportSettings.getTitle());
 	}
-	
-	
+
+
 	private List<Column> createColumns()
 	{
 		final List<Column> columns = new ArrayList<Column>();
-		
+
 		for(final Object id : this.tblColumnChooser.getItemIds())
 		{
 			final Item item = this.tblColumnChooser.getItem(id);
-			
+
 			if(!((CheckBox)item.getItemProperty(this.PROPERTY_SELECTED).getValue()).getValue())
 			{
 				continue;
 			}
-			
+
 			final String columnHeader = (String)item.getItemProperty(this.PROPERTY_COLUMNNAME)
 					.getValue();
 			Integer columnWidth = null;
@@ -332,28 +332,28 @@ public class TableExportPopup
 			catch(final Exception e)
 			{
 			}
-			
-			final Converter<String, Object> converter = this.table.getConverter(id);
-			final Align columnAlignment = this.table.getColumnAlignment(id);
-			final Class<?> type = this.table.getBeanContainerDataSource().getType(id);
-			
+
 			final Object propertyId = item.getItemProperty(this.PROPERTY_COLUMNID).getValue();
+			final Converter<String, Object> converter = this.table.getConverter(propertyId);
+			final Align columnAlignment = this.table.getColumnAlignment(propertyId);
+			final Class<?> type = this.table.getBeanContainerDataSource().getType(propertyId);
+
 			final Column col = new Column(columnHeader,columnWidth,converter,columnAlignment,type,
 					propertyId);
-			
+
 			columns.add(col);
 		}
-		
+
 		return columns;
 	}
-	
-	
+
+
 	protected XdevGridLayout getRoot()
 	{
 		return this.root;
 	}
-	
-	
+
+
 	private void initUI()
 	{
 		this.root = new XdevGridLayout();
@@ -373,7 +373,7 @@ public class TableExportPopup
 		this.cmdClose = new XdevButton();
 		this.cmdSave = new XdevButton();
 		this.cmdShow = new XdevButton();
-		
+
 		this.root.setIcon(FontAwesome.TABLE);
 		this.root.setCaption(StringResourceUtils.localizeString("{$caption}",this));
 		this.txtReportName.setCaption(StringResourceUtils.localizeString("{$title}",this));
@@ -414,7 +414,7 @@ public class TableExportPopup
 		this.cmdShow.setIcon(FontAwesome.EYE);
 		this.cmdShow.setCaption(StringResourceUtils.localizeString("{$show}",this));
 		this.cmdShow.setStyleName("primary");
-		
+
 		this.cmdSelectAll.setWidth(100,Unit.PERCENTAGE);
 		this.cmdSelectAll.setHeight(-1,Unit.PIXELS);
 		this.layoutTableButtons.addComponent(this.cmdSelectAll);
@@ -470,7 +470,7 @@ public class TableExportPopup
 		this.root.setComponentAlignment(this.footer,Alignment.MIDDLE_RIGHT);
 		this.root.setColumnExpandRatio(0,10.0F);
 		this.root.setSizeUndefined();
-		
+
 		this.cmdSelectAll.addClickListener(event -> this.cmdSelectAll_buttonClick(event));
 		this.cmdSelectNone.addClickListener(event -> this.cmdSelectNone_buttonClick(event));
 		this.cbPageType.addValueChangeListener(new Property.ValueChangeListener()
@@ -492,7 +492,7 @@ public class TableExportPopup
 		this.cmdClose.addClickListener(event -> this.cmdClose_buttonClick(event));
 		this.cmdShow.addClickListener(event -> this.cmdShow_buttonClick(event));
 	}
-	
+
 	private XdevGridLayout					root;
 	private XdevButton						cmdSelectAll, cmdSelectNone, cmdClose, cmdSave, cmdShow;
 	private XdevLabel						lblStatus;
